cafe637fbad5952570ffa928a518f170cf6fa25e
vinnieOrdobas <viniordobas@icloud.com>
2021-08-12 17:44:51 +0000

    Added logic for stripe_elements.js

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -50,6 +50,6 @@ def webhook(request):
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
     print('success')
-    # Call the event handler with the event
+    # Call the event handler with the eventI'
     response = event_handler(event)
     return response
