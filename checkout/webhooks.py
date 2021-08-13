fdcb7a8ad1ba5a31c2d34a8375fc935cd443bcee
vinnieOrdobas <viniordobas@icloud.com>
2021-08-13 06:54:02 +0000

    Payment flow complete

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -49,7 +49,6 @@ def webhook(request):
     # If there's a handler for it, get it from the event map
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
-    print('success')
     # Call the event handler with the eventI'
     response = event_handler(event)
     return response
