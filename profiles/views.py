8dcc49b8479f6ad35302e076acd3597aca64e550
vinnieOrdobas <viniordobas@icloud.com>
2021-08-13 16:55:17 +0000

    created profile app and templates

---
+++ profiles/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def profile(request):
+    """ 
+    Display the user's profile
+    """
+    template = 'profiles/propfile.html'
+    context = {}
+
+    return render(request, template, context)
