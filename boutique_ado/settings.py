79600dc55ab8056ecee81e0eec8a0a5acc52570f
vinnieOrdobas <viniordobas@icloud.com>
2021-08-17 10:45:02 +0000

    squished settings.py bug with if conditional, line 126

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -123,7 +123,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    else:
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
