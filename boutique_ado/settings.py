4c39eac6dbd4d2335cbc24c7289a7b009ae335e4
vinnieOrdobas <viniordobas@icloud.com>
2021-08-18 06:42:42 +0000

    configured settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://boutique-ado-vinnie.herokuapp.com', 'localhost']
+
+ALLOWED_HOSTS = ['boutique-ado-vinnie.herokuapp.com', 'localhost']
 
 
 # Application definition
